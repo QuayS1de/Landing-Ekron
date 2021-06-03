47f585f08aac90787566b9272b857318fb8043b1
QuaySide-CP <axel235@mail.ru>
2021-06-03 20:57:15 +0300

    Header/Slider is almost finished

---
+++ main.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    $('.slider').slick({
+        arrows: false, //если не нужны по дизайну
+        dots: false,
+        adaptiveHeight: true,
+        adaptiveWidth: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        responsive: [{
+            breakpoint: 1000,
+            settings: {
+                slidesToShow: 2,
+            }
+        }, {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1,
+                adaptiveHeight: false,
+                adaptiveWidth: true,
+            }
+        }]
+
+    });
+})
