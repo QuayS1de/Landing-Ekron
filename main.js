e75a7b6448db12c589a04f4256978bda7f884e79
QuaySide-CP <axel235@mail.ru>
2021-06-11 19:34:30 +0300

    made quote block

--- main.js
+++ main.js
@@ -74,6 +74,30 @@ document.addEventListener('DOMContentLoaded', function () {
             }]
 
         });
+        $('.slider4').slick({
+            arrows: false, //если не нужны по дизайну
+            dots: false,
+            adaptiveHeight: true,
+            // adaptiveWidth: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            // autoplay: true,
+            autoplaySpeed: 3000,
+            responsive: [{
+                breakpoint: 1000,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }, {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    adaptiveHeight: false,
+                    adaptiveWidth: true,
+                }
+            }]
+
+        });
 
 
 
